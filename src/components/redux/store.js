ecb62119e99348abfe9849a003bc3033a5fed2ce
Andrey9019 <129278140+Andrey9019@users.noreply.github.com>
2023-11-28 01:35:44 +0200

    add new file for project

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,34 +1,45 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { contactsRuduser } from './contactsSlice';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import { filterReducer } from './filtersSlice';
+// import { configureStore } from '@reduxjs/toolkit';
+// import { contactsRuduser } from './contactsSlice';
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+// import { filterReducer } from './filtersSlice';
 
-const persistConfig = {
-  key: 'contacts',
-  storage,
-};
-const persistedReducer = persistReducer(persistConfig, contactsRuduser);
+// const persistConfig = {
+//   key: 'contacts',
+//   storage,
+// };
+// const persistedReducer = persistReducer(persistConfig, contactsRuduser);
+
+// export const store = configureStore({
+//   reducer: {
+//     contacts: persistedReducer,
+//     filter: filterReducer,
+//   },
+//   middleware: getDefaultMiddleware =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     }),
+// });
+// export const persistor = persistStore(store);
+
+import { configureStore } from '@reduxjs/toolkit';
+import { filterSlice } from './filtersSlice';
+import { contactsReducer } from './contactsSlice';
 
 export const store = configureStore({
   reducer: {
-    contacts: persistedReducer,
-    filter: filterReducer,
+    contacts: contactsReducer,
+    filter: filterSlice,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
-export const persistor = persistStore(store);
